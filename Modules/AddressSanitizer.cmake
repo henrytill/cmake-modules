6f851211a15bb50275d2441e023817685952046a
Henry Till <henrytill@gmail.com>
2022-04-13 20:05:19 -0700

    rename build config Asan -> DebugAsan

--- Modules/AddressSanitizer.cmake
+++ Modules/AddressSanitizer.cmake
@@ -3,11 +3,11 @@ include(CheckIncludeFile)
 get_property(isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 
 if(isMultiConfig)
-  if(NOT "Asan" IN_LIST CMAKE_CONFIGURATION_TYPES)
-    list(APPEND CMAKE_CONFIGURATION_TYPES Asan)
+  if(NOT "DebugAsan" IN_LIST CMAKE_CONFIGURATION_TYPES)
+    list(APPEND CMAKE_CONFIGURATION_TYPES DebugAsan)
   endif()
 else()
-  set(allowedBuildTypes Debug Release RelWithDebInfo MinSizeRel Asan)
+  set(allowedBuildTypes Debug Release RelWithDebInfo MinSizeRel DebugAsan)
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "${allowedBuildTypes}")
 
   if(CMAKE_BUILD_TYPE AND NOT CMAKE_BUILD_TYPE IN_LIST allowedBuildTypes)
@@ -15,25 +15,25 @@ else()
   endif()
 endif()
 
-set(CMAKE_C_FLAGS_ASAN
+set(CMAKE_C_FLAGS_DEBUGASAN
     "${CMAKE_C_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer"
-    CACHE STRING "Flags used by the C compiler for Asan build type or configuration." FORCE)
+    CACHE STRING "Flags used by the C compiler for DebugAsan build type or configuration." FORCE)
 
-set(CMAKE_CXX_FLAGS_ASAN
+set(CMAKE_CXX_FLAGS_DEBUGASAN
     "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fno-omit-frame-pointer"
-    CACHE STRING "Flags used by the C++ compiler for Asan build type or configuration." FORCE)
+    CACHE STRING "Flags used by the C++ compiler for DebugAsan build type or configuration." FORCE)
 
-set(CMAKE_EXE_LINKER_FLAGS_ASAN
+set(CMAKE_EXE_LINKER_FLAGS_DEBUGASAN
     "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address"
-    CACHE STRING "Linker flags to be used to create executables for Asan build type." FORCE)
+    CACHE STRING "Linker flags to be used to create executables for DebugAsan build type." FORCE)
 
-set(CMAKE_SHARED_LINKER_FLAGS_ASAN
+set(CMAKE_SHARED_LINKER_FLAGS_DEBUGASAN
     "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} -fsanitize=address"
-    CACHE STRING "Linker lags to be used to create shared libraries for Asan build type." FORCE)
+    CACHE STRING "Linker lags to be used to create shared libraries for DebugAsan build type." FORCE)
 
 ### Some other helpful config
 
-if(${CMAKE_BUILD_TYPE} STREQUAL "Asan")
+if("${CMAKE_BUILD_TYPE}" STREQUAL "DebugAsan")
   SET(ASAN TRUE)
 endif()
 
