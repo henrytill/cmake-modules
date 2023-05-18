694c538a27d19705f32facf1b14493450e10c30f
Henry Till <henrytill@gmail.com>
2023-05-18 14:09:44 -0700

    clean up feature tests

--- has_attribute_cleanup.c
+++ has_attribute_cleanup.c
@@ -1,41 +1,35 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define _cleanup_(f) __attribute__((cleanup(f)))
-
-#define ARRAY_LENGTH(array) (sizeof(array) / sizeof((array)[0]))
-
-#define ARRAY_PRINT(array, len)        \
-  do {                                 \
-    printf(#array " = (%p) {", array); \
-    printf("%d", (array)[0]);          \
-    for (size_t i = 1; i < len; ++i) { \
-      printf(", %d", (array)[i]);      \
-    }                                  \
-    printf("}\n");                     \
+#define ARRAY_PRINT(array, len)          \
+  do {                                   \
+    printf(#array " = (%p) {", array);   \
+    printf("%d", (array)[0]);            \
+    for (size_t i = 1; i < (len); ++i) { \
+      printf(", %d", (array)[i]);        \
+    }                                    \
+    printf("}\n");                       \
   } while (0)
 
 static const int src[] = {0, 1, 2, 3, 4, 5, 6, 7};
 
-static const size_t srclen = ARRAY_LENGTH(src);
+static const size_t srclen = (sizeof(src) / sizeof((src)[0]));
 
-static void cleanup(void *p) {
-  void *ptr = *(void **)p;   /* void *pp = (void **)p */
+static void cleanup(void* p) {
+  void* ptr = *(void**)p;    /* void *pp = (void **)p */
   free(ptr);                 /* free(*pp) */
   printf("free(%p)\n", ptr); /* printf("free(%p)\n", *pp) */
 }
 
-int main(int argc, char **argv) {
+int main(void) {
   extern const int src[];
   extern const size_t srclen;
 
-  (void)argc;
-  (void)argv;
-
   const int dstlen = srclen;
 
-  _cleanup_(cleanup) int *dst = calloc(dstlen, sizeof(*dst));
+  __attribute__((cleanup(cleanup))) int* dst = calloc(dstlen, sizeof(*dst));
   if (dst == NULL) {
     fprintf(stderr, "calloc failed\n");
     return EXIT_FAILURE;
--- has_builtin_types_compatible_p.c
+++ has_builtin_types_compatible_p.c
@@ -5,8 +5,6 @@ typedef uint64_t foo;
 
 _Static_assert(__builtin_types_compatible_p(foo, uint64_t), "foo is not uint64_t");
 
-int main(int argc, char *argv[]) {
-  (void)argc;
-  (void)argv;
+int main(void) {
   return EXIT_SUCCESS;
 }
